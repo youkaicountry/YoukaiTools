b64c52f06f1e91e9a22e1dc19423b77ba487c1ce
Nathaniel Caldwell <nbcwell@gmail.com>
2011-09-06 00:38:27 -0400

    Used another function for atan2angle, and renamed it to atan2_angle.

--- src/YoukaiTools/AdvMath/Trig.py
+++ src/YoukaiTools/AdvMath/Trig.py
@@ -1,8 +1,13 @@
 import math
 
-def atan2angle(y, x):
-    a = math.atan2(y, x)
-    if a < 0:
-        a += 6.28318531
-    return a
-    
+#def atan2angle(y, x):
+#    a = math.atan2(y, x)
+#    if a < 0:
+#        a += 6.28318531
+#    return a
+
+def atan2_angle(y, x):
+    v = math.atan2(y, x)
+    if v < 0:
+        return (math.pi-abs(v)) + math.pi
+    return v
