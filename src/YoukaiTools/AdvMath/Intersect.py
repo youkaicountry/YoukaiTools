a70beebe79cc18bb758f954b2ef33ec0e59a46d6
Nathaniel Caldwell <nbcwell@gmail.com>
2011-09-06 00:18:16 -0400

    Commented out a function that was totally unfinished.

--- src/YoukaiTools/AdvMath/Intersect.py
+++ src/YoukaiTools/AdvMath/Intersect.py
@@ -1,15 +1,16 @@
 from . import Vector
 
+#UNFINISHED
 #intersectpoint = result, vector3
 #normal = the plane's normal
 #scale = float of ?
 #startpoint = vector3 line start?
 #endpoint = vector3 line end?
 #point = vector3 plane?
-lineXPlane(intersectpoint, normal, startpoint, endpoint, point):
-    vec = [0, 0, 0]
-    vec2 = [0, 0, 0]
-    Vector.subtractVectors3(vec, endpoint, point)
-    if Vector.dotProduct3(vec, normal) > 0: return False
-    
-    return
+#def lineXPlane(intersectpoint, normal, startpoint, endpoint, point):
+#    vec = [0, 0, 0]
+#    vec2 = [0, 0, 0]
+#    Vector.subtractVectors3(vec, endpoint, point)
+#    if Vector.dotProduct3(vec, normal) > 0: return False
+#    
+#    return
