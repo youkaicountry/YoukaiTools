649e5cc36492689659019de28cbf11f58bc94543
Nathaniel Caldwell <nbcwell@gmail.com>
2011-09-12 16:37:19 -0400

    Added AnimatedObject and AnimationSet objects, instead of the single Animated class that existed before.

--- src/YoukaiTools/GameTools/Animated/__init__.py
+++ src/YoukaiTools/GameTools/Animated/__init__.py
@@ -18,33 +18,51 @@
 #OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 #SOFTWARE.
 
-#an animated object
-class Animated:
-   def __init__(self):
-      self.frames = {}
-      self.times = {}
-      self.loops = {}
-      self.next = {}
-      
-      self.current_state = 0
-      self.current_frame = 0 #
-      self.current_time = 0
-      self.loop_count = 0
-      
-   def update(self, dt=1):
+class AnimatedObject:
+    def __init__(self, animation_set):
+        self.animation_set = animation_set
+        self.current_state = 0
+        self.current_frame = 0 #
+        self.current_time = 0
+        self.loop_count = 0
+        return
+    
+    def update(self, dt=1):
       #print("frame: " + str(self.current_frame) + " time: " + str(self.current_time))
-      if self.current_time >= self.times[self.current_state][self.current_frame]:
+      if self.current_time >= self.animation_set.times[self.current_state][self.current_frame]:
          self.current_frame += 1
          if self.current_frame >= len(self.frames[self.current_state]):
-            if self.loop_count >= self.loops[self.current_state] and self.loops[self.current_state] >= 0:
+            if self.animation_set.loop_count >= self.animation_set.loops[self.current_state] and self.animation_set.loops[self.current_state] >= 0:
                if self.next[self.current_state] != None:
-                  self.current_state = self.next[self.current_state]
+                  self.current_state = self.animation_set.next[self.current_state]
                   self.loop_count = 0
                   self.current_time = 0
                   self.current_frame = 0
             else: self.loop_count += 1; self.current_time = 0; self.current_frame = 0
       self.current_time += dt
       return self.getSprite()
+
+    def getSprite(self):
+       return self.animation_set.frames[self.current_state][self.current_frame]
+   
+    def setAnimation(self, animation, start_time = 0):
+      self.current_state = animation
+      self.current_frame = 0
+      self.current_time = start_time
+      self.loop_count = 0
+      return
+    
+#an animated object
+class AnimationSet:
+   def __init__(self):
+      self.frames = {}
+      self.times = {}
+      self.loops = {}
+      self.next = {}
+      
+      
+      
+
             
       
    
@@ -66,13 +84,7 @@ class Animated:
       self.next[name] = next_animation
       return
       
-   def setAnimation(self, animation, start_time = 0):
-      self.current_state = animation
-      self.current_frame = 0
-      self.current_time = start_time
-      self.loop_count = 0
-      return
+
    
-   def getSprite(self):
-      return self.frames[self.current_state][self.current_frame]
+   
       
