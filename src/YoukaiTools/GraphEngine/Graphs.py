e11fd2d9cea9b238d8058a7d449847f58219fe8d
Nathan <nbcwell@gmail.com>
2011-09-15 05:10:50 -0400

    Fixed even more whitespace issues.

--- src/YoukaiTools/GraphEngine/Graphs.py
+++ src/YoukaiTools/GraphEngine/Graphs.py
@@ -21,27 +21,27 @@
 from . import BaseObjects
 
 class BasicGraph(BaseObjects.BaseGraph):
-   def __init__(self):
-      self.clearGraph()
+    def __init__(self):
+        self.clearGraph()
 
-   def clearGraph(self):
-      self.vertices = {}
-      self.edges    = {}
-      self.vertextop = 0
-      self.edgetop = 0
+    def clearGraph(self):
+        self.vertices = {}
+        self.edges = {}
+        self.vertextop = 0
+        self.edgetop = 0
 
-   def addVertex(self, vertexid=None):
-      #the list is: connected vertices, connected edges, and data
-      if (vertexid is None): 
-          usev = self.vertextop
-          self.vertextop+=1
-      else:
-          usev = vertexid
-      self.vertices[usev] = [set(), set(), {}]
-      out = self.vertextop
-      return usev
+    def addVertex(self, vertexid=None):
+        #the list is: connected vertices, connected edges, and data
+        if (vertexid is None): 
+            usev = self.vertextop
+            self.vertextop += 1
+        else:
+            usev = vertexid
+        self.vertices[usev] = [set(), set(), {}]
+        out = self.vertextop
+        return usev
   
-   def renameVertex(self, vertexidold, vertexidnew):
+    def renameVertex(self, vertexidold, vertexidnew):
         a = self.vertices[vertexidold]
         del self.vertices[vertexidold]
         self.vertices[vertexidnew] = a
@@ -56,155 +56,156 @@ class BasicGraph(BaseObjects.BaseGraph):
                 self.edges[e][0][1] = vertexidnew
         return
 
-   def addEdge(self, vertex1, vertex2, direction=0, edgeid=None):
-      #the list is: connection info, and data
-      if (edgeid is None): 
-          usev = self.edgetop
-          self.edgetop+=1
-      else:
-          usev = edgeid
-      self.__makeedgeinfo(vertex1, vertex2, direction, usev)
-      return usev
+    def addEdge(self, vertex1, vertex2, direction=0, edgeid=None):
+        #the list is: connection info, and data
+        if (edgeid is None): 
+            usev = self.edgetop
+            self.edgetop += 1
+        else:
+            usev = edgeid
+        self.__makeedgeinfo(vertex1, vertex2, direction, usev)
+        return usev
       
-   def containsVertex(self, vertexid):
-       return vertexid in self.vertices
+    def containsVertex(self, vertexid):
+        return vertexid in self.vertices
        
-   def containsEdge(self, edgeid):
-       return edgeid in self.edges
+    def containsEdge(self, edgeid):
+        return edgeid in self.edges
    
-   #get vertices that connect this one
-   #was getConnections
-   def getAdjacentVertices(self, vertexid):
-      return [x for x in self.vertices[vertexid][0]]
+    #get vertices that connect this one
+    #was getConnections
+    def getAdjacentVertices(self, vertexid):
+        return [x for x in self.vertices[vertexid][0]]
    
-   #get all edges connected to a vertex
-   #was getVertexEdges   
-   def getAdjacentEdges(self, vertexid):
-      return [x for x in self.vertices[vertexid][1]]
+    #get all edges connected to a vertex
+    #was getVertexEdges   
+    def getAdjacentEdges(self, vertexid):
+        return [x for x in self.vertices[vertexid][1]]
    
-   #return [(egdeid, vertexid), ...] for all adjacent connections
-   def getAdjacent(self, vertexid):
-       #return [(x[1], x[0]) for x in self.vertices[vertexid]]
-       return [(e, (self.getEdgeEnd(vertexid, e))) for e in self.vertices[vertexid][1]]
+    #return [(egdeid, vertexid), ...] for all adjacent connections
+    def getAdjacent(self, vertexid):
+        #return [(x[1], x[0]) for x in self.vertices[vertexid]]
+        return [(e, (self.getEdgeEnd(vertexid, e))) for e in self.vertices[vertexid][1]]
 
-   def getVertexData(self, vertexid, index):
-      return self.vertices[vertexid][2][index]
+    def getVertexData(self, vertexid, index):
+        return self.vertices[vertexid][2][index]
 
-   def setVertexData(self, vertexid, index, data):
-      self.vertices[vertexid][2][index] = data
-      return
+    def setVertexData(self, vertexid, index, data):
+        self.vertices[vertexid][2][index] = data
+        return
 
-   def getEdgeData(self, edgeid, index):
-      return self.edges[edgeid][1][index]
+    def getEdgeData(self, edgeid, index):
+        return self.edges[edgeid][1][index]
 
-   def setEdgeData(self, edgeid, index, data):
-      self.edges[edgeid][1][index] = data
-      return
+    def setEdgeData(self, edgeid, index, data):
+        self.edges[edgeid][1][index] = data
+        return
 
-   def removeVertex(self, vertexid):
-      l = [x for x in self.vertices[vertexid][1]]
-      for x in l:
-         print("ll:"+str(x))
-         self.removeEdge(x)
-      del self.vertices[vertexid]
-      return
+    def removeVertex(self, vertexid):
+        l = [x for x in self.vertices[vertexid][1]]
+        for x in l:
+            print("ll:" + str(x))
+            self.removeEdge(x)
+        del self.vertices[vertexid]
+        return
 
-   def removeEdge(self, edgeid):
-      self.__clearedgeinfo(self.edges[edgeid][0][0], edgeid)
-      self.__clearedgeinfo(self.edges[edgeid][0][1], edgeid)
-      del self.edges[edgeid]
-      return
+    def removeEdge(self, edgeid):
+        self.__clearedgeinfo(self.edges[edgeid][0][0], edgeid)
+        self.__clearedgeinfo(self.edges[edgeid][0][1], edgeid)
+        del self.edges[edgeid]
+        return
 
-   def modifyEdge(self, edgeid, vertex1, vertex2, direction=0):
-      oldv1 = self.edges[edgeid][0][0]
-      oldv2 = self.edges[edgeid][0][1]
-      self.__clearedgeinfo(oldv1, edgeid)
-      self.__clearedgeinfo(oldv2, edgeid)
-      self.__makeedgeinfo(vertex1, vertex2, direction, edgeid)
-      return
+    def modifyEdge(self, edgeid, vertex1, vertex2, direction=0):
+        oldv1 = self.edges[edgeid][0][0]
+        oldv2 = self.edges[edgeid][0][1]
+        self.__clearedgeinfo(oldv1, edgeid)
+        self.__clearedgeinfo(oldv2, edgeid)
+        self.__makeedgeinfo(vertex1, vertex2, direction, edgeid)
+        return
 
-   def __makeedgeinfo(self, vertex1, vertex2, direction, edgeid):
-      self.edges[edgeid] = [[vertex1, vertex2, direction], {}]
-      #add the connection information to the vertices
-      if direction == 0:
-         self.vertices[vertex1][0].add(vertex2)
-         self.vertices[vertex1][1].add(edgeid)
-         self.vertices[vertex2][0].add(vertex1)
-         self.vertices[vertex2][1].add(edgeid)
-      elif direction == 1:
-         self.vertices[vertex1][0].add(vertex2)
-         self.vertices[vertex1][1].add(edgeid)
-         self.vertices[vertex2][1].add(edgeid)
-      elif direction == -1:
-         self.vertices[vertex1][1].add(edgeid)
-         self.vertices[vertex2][0].add(vertex1)
-         self.vertices[vertex2][1].add(edgeid)
+    def __makeedgeinfo(self, vertex1, vertex2, direction, edgeid):
+        self.edges[edgeid] = [[vertex1, vertex2, direction], {}]
+        #add the connection information to the vertices
+        if direction == 0:
+            self.vertices[vertex1][0].add(vertex2)
+            self.vertices[vertex1][1].add(edgeid)
+            self.vertices[vertex2][0].add(vertex1)
+            self.vertices[vertex2][1].add(edgeid)
+        elif direction == 1:
+            self.vertices[vertex1][0].add(vertex2)
+            self.vertices[vertex1][1].add(edgeid)
+            self.vertices[vertex2][1].add(edgeid)
+        elif direction == -1:
+            self.vertices[vertex1][1].add(edgeid)
+            self.vertices[vertex2][0].add(vertex1)
+            self.vertices[vertex2][1].add(edgeid)
 
-   def __clearedgeinfo(self, vertexid, edgeid):
-      self.vertices[vertexid][1].remove(edgeid)
-      v1 = self.edges[edgeid][0][0]
-      v2 = self.edges[edgeid][0][1]
-      v = -1
-      if v1 == vertexid: v = v2
-      if v2 == vertexid: v = v1
-      vtest = False
-      for x in self.vertices[vertexid][1]:
-         if self.__isconnected(vertexid, v, x):
-            vtest = True
-      if not vtest:
-         try:
-            self.vertices[vertexid][0].remove(v)
-         except:
-            pass
-      return
+    def __clearedgeinfo(self, vertexid, edgeid):
+        self.vertices[vertexid][1].remove(edgeid)
+        v1 = self.edges[edgeid][0][0]
+        v2 = self.edges[edgeid][0][1]
+        v = -1
+        if v1 == vertexid: v = v2
+        if v2 == vertexid: v = v1
+        vtest = False
+        for x in self.vertices[vertexid][1]:
+            if self.__isconnected(vertexid, v, x):
+                vtest = True
+        if not vtest:
+            try:
+                self.vertices[vertexid][0].remove(v)
+            except:
+                pass
+        return
 
-   def __isconnected(self, sourcev, targetv, edgeid):
-      if self.edges[edgeid][0][2] == 0:
-         if self.edges[edgeid][0][0] == sourcev:
-            if self.edges[edgeid][0][1] == targetv:
-               return True
-         if self.edges[edgeid][0][0] == targetv:
-            if self.edges[edgeid][0][1] == sourcev:
-               return True
-      if self.edges[edgeid][0][2] == 1:
-         if self.edges[edgeid][0][0] == sourcev:
-            if self.edges[edgeid][0][1] == targetv:
-               return True
-      if self.edges[edgeid][0][2] == -1:
-         if self.edges[edgeid][0][0] == targetv:
-            if self.edges[edgeid][0][1] == sourcev:
-               return True
-      return False
+    def __isconnected(self, sourcev, targetv, edgeid):
+        if self.edges[edgeid][0][2] == 0:
+            if self.edges[edgeid][0][0] == sourcev:
+                if self.edges[edgeid][0][1] == targetv:
+                    return True
+            if self.edges[edgeid][0][0] == targetv:
+                if self.edges[edgeid][0][1] == sourcev:
+                    return True
+        if self.edges[edgeid][0][2] == 1:
+            if self.edges[edgeid][0][0] == sourcev:
+                if self.edges[edgeid][0][1] == targetv:
+                    return True
+        if self.edges[edgeid][0][2] == -1:
+            if self.edges[edgeid][0][0] == targetv:
+                if self.edges[edgeid][0][1] == sourcev:
+                    return True
+        return False
 
-   def getVertexDataKeys(self, vertexid):
-      return [x for x in self.vertices[vertexid][2].keys()]
+    def getVertexDataKeys(self, vertexid):
+        return [x for x in self.vertices[vertexid][2].keys()]
       
-   def getEdgeDataKeys(self, edgeid):
-      return [x for x in self.edges[edgeid][1]]
+    def getEdgeDataKeys(self, edgeid):
+        return [x for x in self.edges[edgeid][1]]
 
-   #should return a list (vertex1, vertex2, direction)
-   def getEdgeInfo(self, edgeid, normalized=False):
-       v1, v2, d = self.edges[edgeid][0]
-       if normalized:
-           if (d == 1) or d == 0:
-               return (v1, v2, d)
-           return (v2, v1, 1)
-       else:
-           return (v1, v2, d)
+    #should return a list (vertex1, vertex2, direction)
+    def getEdgeInfo(self, edgeid, normalized=False):
+        v1, v2, d = self.edges[edgeid][0]
+        if normalized:
+            if (d == 1) or d == 0:
+                return (v1, v2, d)
+            return (v2, v1, 1)
+        else:
+            return (v1, v2, d)
 
-   def getVertexList(self):
-      return self.vertices.keys()
+    def getVertexList(self):
+        return self.vertices.keys()
 
-   def getEdgeList(self):
-      return self.edges.keys()
+    def getEdgeList(self):
+        return self.edges.keys()
+     
+    def getOrder(self):
+        return len(self.vertices)
    
-   def getOrder(self):
-       return len(self.vertices)
+    def getSize(self):
+        return len(self.edges)
    
-   def getSize(self):
-       return len(self.edges)
-   
-   def getEdgeEnd(self, vertexid, edgeid):
+    def getEdgeEnd(self, vertexid, edgeid):
         ei = self.edges[edgeid][0]
         if ei[0] == vertexid: return ei[1]
         else: return ei[0]
+
--- src/YoukaiTools/MazeGen/generators/maze.py
+++ src/YoukaiTools/MazeGen/generators/maze.py
@@ -25,64 +25,64 @@ from .. import convert
 #a recursive backtracker maze
 #takes the actual maze size, the out put is that size*2 + 1
 class recursiveBacktrackMaze():
-   def __init__(self, r=random.Random()):
-      self.maze = None
-      self.found = None
-      self.path = None
-      self.converted = None
-      self.blocked = None
-      self.r = r
-      self.nx = 0
-      self.ny = 0
+    def __init__(self, r=random.Random()):
+        self.maze = None
+        self.found = None
+        self.path = None
+        self.converted = None
+        self.blocked = None
+        self.r = r
+        self.nx = 0
+        self.ny = 0
       
-   #param = "blockmap" -> returns a blockmap
-   #param = "mazemap" -> return a mazemap
-   def get(self, outputtype):
-       if outputtype == "blockmap":
-           if self.converted != None: return self.converted
-           self.converted = convert.mazeMap2BlockMap(self.maze, self.blocked)
-           return self.converted
-       elif outputtype == "mazemap":
-           return self.maze
-       return None
-   
-   def __recursiveBacktrack(self, x, y, blocked, unblocked=None):
-      nx = x
-      ny = y
-      self.path.append((x, y))
-      while len(self.path) > 0:
-         self.found.setSpace(nx, ny, blocked)
-         n = self.maze.getAllNeighbors(nx, ny)
-         #print "OUT: "+str(n)
-         realn = []
-         for o in n:
-            if self.found.getSpace(o[0], o[1]) is None:
-               realn.append(o)
-         if len(realn) > 0:
-            self.path.append((nx, ny))
-            #self.r.shuffle(realn)
-            choice = self.r.randint(0, len(realn)-1)
-            if self.found.getSpace(realn[choice][0], realn[choice][1]) is None:
-               dir = self.maze.getDirection(nx, ny, realn[choice][0], realn[choice][1])
-               self.maze.setWall(nx, ny, dir, None)
-               nx = realn[choice][0]
-               ny = realn[choice][1]
-               continue
-         else:
-            if len(self.path) == 0: break
+    #param = "blockmap" -> returns a blockmap
+    #param = "mazemap" -> return a mazemap
+    def get(self, outputtype):
+        if outputtype == "blockmap":
+            if self.converted != None: return self.converted
+            self.converted = convert.mazeMap2BlockMap(self.maze, self.blocked)
+            return self.converted
+        elif outputtype == "mazemap":
+            return self.maze
+        return None
+    
+    def __recursiveBacktrack(self, x, y, blocked, unblocked=None):
+        nx = x
+        ny = y
+        self.path.append((x, y))
+        while len(self.path) > 0:
+            self.found.setSpace(nx, ny, blocked)
+            n = self.maze.getAllNeighbors(nx, ny)
+            #print "OUT: "+str(n)
+            realn = []
+            for o in n:
+                if self.found.getSpace(o[0], o[1]) is None:
+                    realn.append(o)
+            if len(realn) > 0:
+                self.path.append((nx, ny))
+                #self.r.shuffle(realn)
+                choice = self.r.randint(0, len(realn)-1)
+                if self.found.getSpace(realn[choice][0], realn[choice][1]) is None:
+                    dir = self.maze.getDirection(nx, ny, realn[choice][0], realn[choice][1])
+                    self.maze.setWall(nx, ny, dir, None)
+                    nx = realn[choice][0]
+                    ny = realn[choice][1]
+                    continue
             else:
-               a = self.path.pop(len(self.path)-1)
-               nx = a[0]
-               ny = a[1]
-               continue
-      return
+                if len(self.path) == 0: break
+                else:
+                    a = self.path.pop(len(self.path)-1)
+                    nx = a[0]
+                    ny = a[1]
+                    continue
+        return
       
-   #parameters contains (width, height, startx, starty, blocked)
-   def make(self, width, height, startx=0, starty=0, blocked=0, unblocked=None):
-      self.path = []
-      self.blocked = blocked
-      self.found = datatypes.BlockMap(width, height, unblocked)
-      self.maze = datatypes.MazeMap(width, height, blocked)
-      self.found = datatypes.BlockMap(width, height, unblocked)
-      self.__recursiveBacktrack(startx, starty, blocked)
-      return
+    #parameters contains (width, height, startx, starty, blocked)
+    def make(self, width, height, startx=0, starty=0, blocked=0, unblocked=None):
+        self.path = []
+        self.blocked = blocked
+        self.found = datatypes.BlockMap(width, height, unblocked)
+        self.maze = datatypes.MazeMap(width, height, blocked)
+        self.found = datatypes.BlockMap(width, height, unblocked)
+        self.__recursiveBacktrack(startx, starty, blocked)
+        return
