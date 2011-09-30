3285c545b23e93fc243aab79bcd93d5c1b223b26
Nathaniel Caldwell <nbcwell@gmail.com>
2011-09-30 15:57:47 -0400

    Updated the keymanager unit test.

--- src/UnitTests/GameTools/test_keymanager.py
+++ src/UnitTests/GameTools/test_keymanager.py
@@ -9,12 +9,24 @@ class TestKeyManager(unittest.TestCase):
     def tearDown(self):
         return
     
-class TestBasicKeys(TestKeyManager):
-    def test_basic(self):
-        self.assertEqual(len(self.km.update_from_keyset(keyset=set())), 0)
-        self.km.update_from_keyset(keyset=set(["b", "d"])) #b_down, d_down
-        self.km.update_from_keyset(keyset=set(["b", "d", "e"])) #e_down
-        self.km.update_from_keyset(keyset=set(["b", "e"])) #d_up
+class TestBasicKeystate(TestKeyManager):
+    def test_basic_keystate(self):
+        v = self.km.update_from_keyset(keyset=set())
+        self.assertEqual(len(v), 0)
+        self.assertItemsEqual(v, [])
+        
+        v = self.km.update_from_keyset(keyset=set(["b", "d"])) #b_down, d_down
+        self.assertEqual(len(v), 2)
+        self.assertItemsEqual(v, ["d_down", "b_down"])
+        
+        v = self.km.update_from_keyset(keyset=set(["b", "d", "e"])) #e_down
+        self.assertEqual(len(v), 1)
+        self.assertItemsEqual(v, ["e_down"])
+        
+        v = self.km.update_from_keyset(keyset=set(["b", "e"])) #d_up
+        self.assertEqual(len(v), 1)
+        self.assertItemsEqual(v, ["d_up"])
+        
         self.km.update_from_keyset(keyset=set(["b"])) #e_up
         self.km.update_from_keyset(keyset=set(["a"])) #a_down, b_up
         self.km.update_from_keyset(keyset=set()) #a_up
