1724df7032845c615176a6785e50ef2395447934
Nathaniel Caldwell <nbcwell@gmail.com>
2011-10-09 19:24:23 -0400

    Added to the neuron test a bit.

--- src/UnitTests/Neuron/test_neuron.py
+++ src/UnitTests/Neuron/test_neuron.py
@@ -11,11 +11,20 @@ class TestKeyManager(unittest.TestCase):
         self.assertAlmostEqual(inv.getOutput("0"), -1.2)
         self.assertAlmostEqual(inv.getOutput("1"), 2.56)
         self.assertEqual(inv.getOutput("2"), 0)
-        print(inv.outputs)
         return
 
 class TestBreadBoard(unittest.TestCase):
     def test_breadboard(self):
+        print("bread")
+        inv1 = Neuron.Neurons.Math.InvertNeuron(2)
+        inv2 = Neuron.Neurons.Math.InvertNeuron(3)
+        
+        bbneurons = {"inv1":inv1, "inv2":inv2}
+        bbinputs = {"inv1.0":"0", "inv1.1":"1", "inv2.2":"2"}
+        bboutputs = {"inv2.0":"0", "inv2.1":"1", "inv2.2":"2"}
+        bbconstants = {}
+        bbwires=[("inv1.0", "inv2.0"), ("inv1.0", "inv2.1")]
+        bb = Neuron.Neurons.Cases.BreadBoard(bbneurons, bbinputs, bboutputs, bbconstants, bbwires)
         return
     
 if __name__ == '__main__':
