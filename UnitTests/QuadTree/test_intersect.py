c5cb85601b80a570e0b017f1b508122ce93c6b67
Nathaniel Caldwell <nbcwell@gmail.com>
2011-10-18 03:19:27 -0400

    Added a file for QuadTree unit tests.

---
+++ UnitTests/QuadTree/test_intersect.py
@@ -0,0 +1,11 @@
+import YoukaiTools.AdvMath
+from YoukaiTools.AdvMath import Intersect as Intersect
+import unittest
+
+class TestRect(unittest.TestCase):
+    def test_rectXRect(self):
+        return
+        
+if __name__ == '__main__':
+    unittest.main()
+    
