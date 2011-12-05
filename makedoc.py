5f2f037b2a7557f576ecc7de3a4c5815728656c0
Nathaniel Caldwell <nbcwell@gmail.com>
2011-12-05 02:34:15 -0500

    Made a script to generate documentation, and removed the epydocgui project file.

---
+++ makedoc.py
@@ -0,0 +1,2 @@
+import os
+os.system("epydoc --html -o doc src/YoukaiTools/GraphEngine/BaseObjects.py src/YoukaiTools/GraphEngine/Graphs.py src/YoukaiTools/GraphEngine/__init__.py src/YoukaiTools/__init__.py")
